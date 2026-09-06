0cb17211ef5a476f8fe4cf621aab9f4900546c47
bdwebb11 <bdwebb11@gmail.com>
2026-09-06 15:48:45 -0500

    Add hall service worker

---
+++ sw.js
@@ -0,0 +1,52 @@
+const VERSION = "dt-hall-v1";
+const SHELL = [
+  "./",
+  "./index.html",
+  "./css/hall.css",
+  "./js/app.js",
+  "./manifest.webmanifest",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(VERSION).then((cache) => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== VERSION).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (url.pathname.endsWith("/data/heroes.json")) {
+    event.respondWith(
+      fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(VERSION).then((cache) => cache.put(req, copy));
+          return res;
+        })
+        .catch(() => caches.match(req))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then((hit) => hit || fetch(req).then((res) => {
+      if (res.ok && res.type === "basic") {
+        const copy = res.clone();
+        caches.open(VERSION).then((cache) => cache.put(req, copy));
+      }
+      return res;
+    }))
+  );
+});
